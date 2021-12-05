dc991f45e0929e8f80cdc64d28dbb609877ef4be
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-12-05 13:52:25 -0300

    route to mark as done todo

--- src/index.js
+++ src/index.js
@@ -76,7 +76,14 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const todo = user.todos.find((todo) => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: "todo not found" });
+  }
+  todo.done = true;
+  return response.json(todo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
